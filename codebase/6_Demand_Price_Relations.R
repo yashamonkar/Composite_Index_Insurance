a5c57f4e86ad053c0d23650aea898a6d1648d5cd
yashamonkar <39978782+yashamonkar@users.noreply.github.com>
2025-02-25 14:25:55 -0500

    Demand-Price Relations

--- codebase/6_Demand_Price_Relations.R
+++ codebase/6_Demand_Price_Relations.R
@@ -11,11 +11,16 @@ library(zoo)
 library(dplyr)
 
 #______________________________________________________________________________#
+#Simulation abronmality
+rm_sim <- 101 #Remove the 101 simulation run
+
 ###Demand Data###
 Demand <- read.csv("data/Simulated_Demand.csv", header = FALSE)
 Demand <- matrix(Demand$V1, nrow = 8760, ncol = 500)
 Demand <- as.data.frame(Demand)
 max_demand <- apply(Demand, 2, max)
+max_demand <- max_demand[-rm_sim]
+
 
 #Daily Steps
 daily_demand <- sapply(Demand, function(column) rollapply(column, width = 24, by = 24, FUN = mean, align = "left", partial = TRUE))
@@ -24,6 +29,7 @@ daily_demand <- as.numeric(unlist(daily_demand))
 
 #Annual Time Steps
 yearly_demand <- colMeans(Demand)
+yearly_demand <- yearly_demand[-rm_sim]
 Demand <- NULL
 
 
@@ -33,6 +39,7 @@ max_price <- apply(Price, 2, max)
 daily_price <- sapply(Price, function(column) rollapply(column, width = 24, by = 24, FUN = mean, align = "left", partial = TRUE))
 daily_price <- as.numeric(unlist(daily_price))
 annual_price <- colMeans(Price)
+annual_price <- annual_price[-rm_sim]
 
 
 #Max Temperature -- Daily
@@ -43,6 +50,7 @@ Temp = data.frame(Temp = rowMeans(Temp),
                   Year = rep(1:500, each = 365))
 Temp = Temp %>% group_by(Year) %>% summarize(max(Temp))
 max_temp = Temp$`max(Temp)`
+max_temp <- max_temp[-rm_sim]
 Temp = NULL
 
 #CDD
@@ -50,6 +58,7 @@ CDD <- read.csv("data/CDD.csv")
 pge_cities = c('FRESNO_T', 'SACRAMENTO_T','SAN.JOSE_T', 'SAN.FRANCISCO_T')
 CDD = CDD[,pge_cities]
 CDD = rowMeans(CDD)
+CDD = CDD[-rm_sim]
 
 
 #Streamflow
@@ -57,18 +66,20 @@ streamflow <- read.csv("data/Streamflow.csv")
 sites = c('ORO_fnf', 'SHA_fnf', 'FOL_fnf', 'PAR_fnf', 'NML_fnf', 'MIL_fnf', 'PFT_fnf')
 streamflow = streamflow[,sites]
 streamflow = rowMeans(streamflow)
+streamflow = streamflow[-rm_sim]
 
 #Net Revenue
 net_revenue <- read.csv("sims/Net_Revenue_no_tax.csv", header = TRUE, sep=",")
 
 #Naural Gas
 Yearly_gas <- read.csv("data/Yearly_gas.csv", header = FALSE)
+Yearly_gas <- Yearly_gas[-rm_sim,]
 
 plt_dataset <- data.frame(Demand = yearly_demand, 
                           streamflow = streamflow,
                           CDD = CDD,
                           Price = annual_price,
-                          NG = Yearly_gas$V1,
+                          NG = Yearly_gas,
                           Revenue = net_revenue$Net_revenue)
 
 quants <- quantile(plt_dataset$streamflow, probs = c(0, 0.2, 0.4, 0.6, 0.8, 1))
